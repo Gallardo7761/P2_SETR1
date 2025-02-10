fc540c902870b18272c3b57411229f616f6c0674
Jose <jomaamga@outlook.es>
2025-02-10 09:11:58 +0100

    Fase 2 FIN

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -148,7 +148,9 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	counter++;
+
+	// TAREA A
+    counter++;
 	if (counter == 10 && state == 0)
 	{
 		moveToXY(1,7); //sitúa el cursor en la posición fila 1 columna 7, primer muñeco
@@ -171,6 +173,7 @@ int main(void)
 		state = 0;
 	}
 
+	// TAREA B
 	if (LL_GPIO_IsInputPinSet(BUTTON_EXTI13_GPIO_Port, BUTTON_EXTI13_Pin))
 		LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
 	else
